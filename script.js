0500a281f7491d87d3a954e54fd4530c20c04528
unknown <Baxtiyorddushaev@gmail.com>
2023-08-20 23:34:51 +0500

    js.dz

---
+++ script.js
@@ -0,0 +1,15 @@
+let str = prompt('Vvedite svoe imya!'),
+  age = +prompt('Vvedite vozrast'),
+  slojeniye = +prompt('2 + 2 ='),
+  vichitaniye = +prompt('2 - 2 ='),
+  umnojeniye = +prompt('2 * 2 ='),
+  deleniye = +prompt('2 / 2 ='),
+  cons = alert('Zaydite v console!');
+
+  console.log(str + ' vash vozrast ' + age + ' pravilniy otver ' + 4 + ' vash otvet ' + slojeniye);
+  console.log(str + ' vash vozrast ' + age + ' pravilniy otver ' + 0 + ' vash otvet ' + vichitaniye);
+  console.log(str + ' vash vozrast ' + age + ' pravilniy otver ' + 4 + ' vash otvet ' + umnojeniye);
+  console.log(str + ' vash vozrast ' + age + ' pravilniy otver ' + 0 + ' vash otvet ' + deleniye);
+
+
+
